eea95d313cc8c7b3fadb66745c795745af72dc79
paigeandersonn <anderson.paige@ufl.edu>
2025-10-23 21:43:49 -0400

    remove community images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -129,7 +129,7 @@ export function About({ onGetInvolvedClick, onContactClick }: AboutProps) {
         </div>
       </section>
 
-      {/* Photo Grid */}
+      {/* Photo Grid 
       <section className="bg-[#f0f4ff] py-12 md:py-20">
         <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-gradient-to-r from-[#0021A5]/10 to-[#FA4616]/10 rounded-xl p-4 mb-8">
@@ -184,7 +184,7 @@ export function About({ onGetInvolvedClick, onContactClick }: AboutProps) {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Call to Action */}
       <section className="bg-[#f0f4ff] py-12 md:py-16">
