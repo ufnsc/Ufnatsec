7a0ad60805589d5a810783db09d48c8a0a746cfb
paigeandersonn <anderson.paige@ufl.edu>
2025-10-23 20:58:58 -0400

    logo update again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,13 +20,11 @@ export function Hero({ onGetInvolvedClick }: HeroProps) {
       <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 py-20 md:py-32 relative z-10">
         <div className="text-center">
           {/* Logo Placeholder */}
-          <div className="mb-6 md:mb-8 inline-flex justify-center">
-            {/*<div className="inline-flex items-center justify-center w-28 h-28 md:w-36 md:h-36 bg-gradient-to-br from-[#0021A5]/10 to-[#FA4616]/10 rounded-full shadow-lg border-4 border-white backdrop-blur-sm relative">
-              <div> className="absolute inset-0 bg-gradient-to-br from-[#0021A5]/5 to-[#FA4616]/5 rounded-full animate-pulse"></div> */}
+          <div className="flex flex-col items-center justify-center mb-10 md:mb-12">
                 <img
                   src="/NatSecLogo.png"
                   alt="UF NatSec Logo"
-                  className="w-40 h-40 md:w-48 md:h-48"
+                  className="w-32 h-32 md:w-40 md:h-40"
                 />
             </div>
 
