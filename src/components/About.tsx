7c8f397797873a764fcb28482a110008ff2e5270
paigeandersonn <anderson.paige@ufl.edu>
2025-10-23 22:51:18 -0400

    remove our story for now

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,7 +62,7 @@ export function About({ onGetInvolvedClick, onContactClick }: AboutProps) {
         </div>
       </section>
 
-      {/* Our Story */}
+      {/* Our Story 
       <section className="bg-[#f0f4ff] py-12 md:py-20">
         <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-gradient-to-r from-[#0021A5]/10 to-[#FA4616]/10 rounded-xl p-4 mb-8">
@@ -95,7 +95,7 @@ export function About({ onGetInvolvedClick, onContactClick }: AboutProps) {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Our Values */}
       <section className="bg-[#f0f4ff] py-12 md:py-20">
