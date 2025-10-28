45580d7dca497871aff540797df2da2c4b42427d
paigeandersonn <anderson.paige@ufl.edu>
2025-10-28 10:01:10 -0400

    update slack icon

--- src/components/GetInvolved.tsx
+++ src/components/GetInvolved.tsx
@@ -188,7 +188,7 @@ export function GetInvolved() {
               LinkedIn
             </Button>
             <Button
-              className="!bg-[#611F69] hover:bg-[#4A154B]/90 text-white rounded-xl w-full sm:w-auto"
+              className="!bg-[#4A154B] hover:bg-[#3F0E40] text-white rounded-xl w-full sm:w-auto"
               onClick={() => window.open('https://join.slack.com/t/ufnatsec/shared_invite/zt-3gnhxlssn-JQbxI3jCo2JZjTtX~Dpl8Q', '_blank')}
               aria-label="Slack"
             >
