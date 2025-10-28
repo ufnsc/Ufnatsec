ee0d1ec76a1443b3adafe66e3b2a18ee263ccdcb
paigeandersonn <anderson.paige@ufl.edu>
2025-10-28 14:28:13 -0400

    sizing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ export function Hero({ onGetInvolvedClick }: HeroProps) {
                 <img
                   src="/NatSecLogo.png"
                   alt="UF NatSec Logo"
-                  className="w-32 h-32 md:w-36 md:h-36 scale-105 md:scale-100"
+                  className="w-32 h-32 md:w-36 md:h-36 scale-200 md:scale-205"
                 />
             </div>
 
