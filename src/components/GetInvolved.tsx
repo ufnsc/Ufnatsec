5aa0534c2fab695cffe5a0f2217a71391fe0f8f5
paigeandersonn <anderson.paige@ufl.edu>
2025-10-27 23:07:04 -0400

    involved page changes

--- src/components/GetInvolved.tsx
+++ src/components/GetInvolved.tsx
@@ -2,6 +2,7 @@ import { Card } from './ui/card';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Calendar, Linkedin, Instagram, Mail } from 'lucide-react';
+import { FaSlack } from 'react-icons/fa';
 import { useState } from 'react';
 
 export function GetInvolved() {
@@ -189,6 +190,7 @@ export function GetInvolved() {
             <Button
               className="bg-[#5865F2] hover:bg-[#5865F2]/90 text-white rounded-xl w-full sm:w-auto"
               onClick={() => window.open('https://join.slack.com/t/ufnatsec/shared_invite/zt-3gnhxlssn-JQbxI3jCo2JZjTtX~Dpl8Q', '_blank')}
+              aria-label="Slack"
             >
               <svg
                 className="mr-2"
@@ -200,7 +202,7 @@ export function GetInvolved() {
               >
                 <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z" />
               </svg>
-              Discord
+              Slack
             </Button>
             <Button
               className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white rounded-xl w-full sm:w-auto"
