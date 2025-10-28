ada9aade0e701c0be504a1ae302a41bb44d2cea0
paigeandersonn <anderson.paige@ufl.edu>
2025-10-28 14:21:45 -0400

    trying size changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ export function Hero({ onGetInvolvedClick }: HeroProps) {
                 <img
                   src="/NatSecLogo.png"
                   alt="UF NatSec Logo"
-                  className="w-40 h-40 md:w-40 md:h-40"
+                  className="w-36 h-36 md:w-40 md:h-40"
                 />
             </div>
 
